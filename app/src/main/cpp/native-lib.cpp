5e6a0eeb7cefc252e19cd9cbd4fcc92fb86b7db5
Aldrin <m13324538952_1@163.com>
2019-04-02 22:56:55 +0800

    修复bug

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,7 +8,7 @@
 #define TAG "Aldrin" // 这个是自定义的LOG的标识
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_aldrin_antidebugtracerpid_MainActivity_stringFromJNI(
+Java_com_aldrin_antidebug_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
